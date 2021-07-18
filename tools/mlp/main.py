7c7ebf436e9156b4a9919dd257fa49f50f7f2f8e
Filippo <ondaradiofm@gmail.com>
2021-07-18 17:57:03 +0200

    Better tooling

--- tools/mlp/main.py
+++ tools/mlp/main.py
@@ -1,68 +1,138 @@
-import numpy as np
-import ROOT as rt
-import matplotlib.pyplot as plt
-import os
-import csv
-import math
 from array import array
-from sklearn.linear_model import LinearRegression
-from sklearn.multioutput import MultiOutputRegressor
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from scipy.stats import norm
-
 from sklearn.neural_network import MLPRegressor
-from sklearn.preprocessing import StandardScaler
+import ROOT as rt
+import numpy as np
 
-INPUT_PATH = "/home/work/Desktop/100-200.root"
-#INPUT_PATH = "/media/work/Waveforms/run4/analysis/amplitudes/100-200.root"
+IN_PATH = "/media/work/Waveforms/run4/analysis/amplitudes/new/100-200-2.root"
+CHANNELS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-FIRST_CHANNEL = 0
-LAST_CHANNEL = 10
+START_X = 0
+END_X = 1000
+STEP_X = 10
 
-NUM_CHANNELS = LAST_CHANNEL - FIRST_CHANNEL
+START_Y = 0
+END_Y = 1000
+STEP_Y = 10
 
-def train(x, y):
-    print(y)
-    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, test_size=0.3)
-    reg = MLPRegressor(hidden_layer_sizes=(100), random_state=1, max_iter=500).fit(x_train, y_train)
-
-    y_test_predicted = reg.predict(x_test)
-    print("Test Score: "+ str(reg.score(x_test, y_test)))
-
-    y_test_resolution = y_test - y_test_predicted
-    print(y_test_resolution)
+WITHIN_STD = 1
 
 def init():
-    file = rt.TFile.Open(INPUT_PATH, "READ")
-    data = file.Get("data")
+    data = [[[] for j in range(101)] for i in range(101)]
 
-    # Input tree setup
+    file = rt.TFile.Open(IN_PATH, "READ")
+    tree = file.Get("data")
+
+    values = {}
+    for channel in CHANNELS:
+        value = array("d", [0.0])
+        tree.SetBranchAddress("amp{}".format(channel), value)
+        values[channel] = value
+
     position = rt.std.vector("double")()
-    data.SetBranchAddress("pos", position)
+    tree.SetBranchAddress("pos", position)
 
-    amplitudes = []
-    for chn in range(FIRST_CHANNEL, LAST_CHANNEL + 1):
-        amplitudes.append(array("d", [0.0]))
-        data.SetBranchAddress("amp{}".format(chn), amplitudes[chn])
+    print("Loading data...", end = "\n\n")
 
-    points = data.GetEntries()
-    out = np.zeros((points, NUM_CHANNELS + 1))
-    xy = np.zeros((points, 2))
-    for point in range(points):
-            data.GetEntry(point)
-            out[point] = np.array([amplitude[0] for amplitude in amplitudes])
-            xy[point] = np.array(position)
+    entries = tree.GetEntries()
+    for entry in range(entries):
+        tree.GetEntry(entry)
 
-            if point % 100 == 0:
-                print("Progress: {}/{}".format(point, points))
+        x, y = int(position[0] / 10), int(position[1] / 10)
 
-    data.ResetBranchAddresses()
+        output = []
+        for channel in CHANNELS:
+            output.append(values[channel][0])
+
+        data[x][y].append(output)
+        if(entry % 100000 == 0):
+            print("Progress: {}/{} events".format(entry, entries))
+
     file.Close()
 
-    out = np.nan_to_num(out)
-    train(out, xy)
+    print("\nApplying corrections... ", end = "")
+
+    highest = 0
+
+    data = np.asarray(data)
+    height, width = data.shape
+    for x in range(height):
+        for y in range(width):
+            # Remove extraneous peaks
+            channels = np.asarray(data[x][y])
+            local = np.amax(channels)
+            highest = local if local > highest else highest
+
+            samples = channels.transpose()
+            means = [np.mean(channel) for channel in samples]
+            stds = [np.std(channel) for channel in samples]
+
+            entries, depth = channels.shape
+            new = []
+            for entry in range(entries):
+                delete = False
+                for chn in range(depth):
+                    mean = means[chn]
+                    std = stds[chn]
+
+                    value = channels[entry][chn]
+                    if np.abs(mean - value) > WITHIN_STD * std:
+                        delete = True
+                        break
+
+                if not delete:
+                    new.append(channels[entry])
+
+            # print("Done at ({}, {})".format(x, y))
+            data[x][y] = new
+
+    print("Done!", end = "")
+    print("\nBuilding vectors... ", end = "")
+
+    a = [] # Coordinates
+    b = [] # Peaks
+
+    for x in range(height):
+        for y in range(width):
+            entries = np.asarray(data[x][y])
+            for entry in entries:
+                add = False
+                new = []
+                for e in entry.tolist():
+                    if e > 120:
+                        new.append(e / highest)
+                        add = True
+                    else:
+                        new.append(0)
+                if add:
+                    b.append(new)
+                    a.append([x, y])
+
+    print("Done!", end = "")
+    print("\nStarting training... ", end = "")
+
+    train(b, a)
+
+def train(x, y):
+    np.random.seed(1)
+    xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size = 0.25,
+        random_state = 42)
+    regressor = MLPRegressor(hidden_layer_sizes = (7), random_state = 42,
+        activation = "logistic", max_iter = 1000).fit(xTrain, yTrain)
+
+    print("Done!\n")
+
+    print("Test score: {:.2f}".format(regressor.score(xTest, yTest)))
+    yPredicted = regressor.predict(xTest)
+
+    resolution = np.absolute(np.asarray(yTest - yPredicted))
+    best = (np.partition(-resolution, 5))[0:5]
+    worst = (-np.partition(-resolution, 5))[:5]
+    average = np.mean(resolution)
+
+    print("Best resolution: {}".format(best))
+    print("Average resolution: {}".format(average))
+    print("Worst resolution: {}".format(worst))
 
 if __name__ == "__main__":
     init()
--- tools/mlp/preprocessing.cpp
+++ tools/mlp/preprocessing.cpp
@@ -5,7 +5,9 @@
 #include <algorithm>
 
 const std::string IN_PATH = "/media/work/Waveforms/run4/100-200.root";
-const std::string OUT_PATH = "/media/work/Waveforms/run4/analysis/amplitudes/new/100-200.root";
+const std::string OUT_PATH = "/media/work/Waveforms/run4/analysis/amplitudes/new/100-200-2.root";
+
+const bool APPEND = false;
 
 // The first channel to consider...
 const int FIRST_CHANNEL = 0;
@@ -21,27 +23,63 @@ const int AFTER_PEAK = 3;
 const int NUM_AVERAGE = 100;
 // Reciprocal of frequency
 const double TIME_STEP = 2E-10;
+// Max value for fit chi2 to accept result
+const double MAX_CHI2 = 1.5;
 
 // Don't touch!
-const int NUM_CHANNELS = LAST_CHANNEL - FIRST_CHANNEL;
+const int NUM_CHANNELS = 1 + LAST_CHANNEL - FIRST_CHANNEL;
 const int NUM_SAMPLES = BEFORE_PEAK + AFTER_PEAK + 1;
+
+template<typename T>
+struct square {
+    T operator()(const T& first, const T& second) const {
+        return (first + second * second);
+    }
+};
+
+double getRMS(std::vector<double>* samples) {
+    return std::accumulate(samples->end() - NUM_AVERAGE,
+        samples->end(), 0, square<double>()) / NUM_AVERAGE;
+}
+
+double getMean(std::vector<double>* samples) {
+    return std::accumulate(samples->end() - NUM_AVERAGE,
+        samples->end(), 0) / NUM_AVERAGE;
+}
+
+std::vector<double> getInterestZone(std::vector<double>* samples) {
+    int peak = std::max_element(samples->begin(),
+        samples->end()) - samples->begin();
+    auto begin = samples->begin() + peak - BEFORE_PEAK;
+    auto end = samples->begin() + peak + AFTER_PEAK + 1;
+    std::vector<double> event(begin, end);
+
+    return event;
+}
 
 void analyze(TTree *in, TTree *out) {
     // Output tree setup
-    std::vector<double> position(2, 0);
-    std::vector<double>* ptr = &position;
-    out->Branch("pos", ptr);
+    std::vector<double>* position = nullptr;
+    if(APPEND) {
+        out->SetBranchAddress("pos", &position);
+    } else {
+        out->Branch("pos", &position);
+    }
 
     std::vector<double> amplitudes(NUM_CHANNELS, 0);
     for(int i = FIRST_CHANNEL; i <= LAST_CHANNEL; i++) {
         std::string amp = "amp" + std::to_string(i);
-        out->Branch(amp.c_str(), &(amplitudes[i - FIRST_CHANNEL]));
+        if(APPEND) {
+            out->SetBranchAddress(amp.c_str(), &(amplitudes[i - FIRST_CHANNEL]));
+        } else {
+            out->Branch(amp.c_str(), &(amplitudes[i - FIRST_CHANNEL]));
+        }
     }
 
     // Input tree setup
-    in->SetBranchAddress("pos", &ptr);
+    in->SetBranchAddress("pos", &position);
 
-    std::vector<std::vector<double>*> channels(NUM_CHANNELS, 0);
+    std::vector<std::vector<double>*> channels(NUM_CHANNELS, nullptr);
     for(int i = FIRST_CHANNEL; i <= LAST_CHANNEL; i++) {
         std::string chn = "w" + std::to_string(i);
         in->SetBranchAddress(chn.c_str(), &(channels[i - FIRST_CHANNEL]));
@@ -52,7 +90,9 @@ void analyze(TTree *in, TTree *out) {
         time.push_back(i * TIME_STEP);
     }
 
-    TF1 *fit = new TF1("fit", "gaus");
+    TF1 fit("fit", "gaus");
+    fit.SetParameter(1, NUM_SAMPLES * TIME_STEP / 2);
+    fit.SetParLimits(1, 0, NUM_SAMPLES * TIME_STEP);
 
     int events = in->GetEntries();
     // Branches interation
@@ -60,20 +100,23 @@ void analyze(TTree *in, TTree *out) {
         in->GetEntry(i);
 
         for(int j = FIRST_CHANNEL; j <= LAST_CHANNEL; j++) {
-            std::vector<double>* channel = channels[j - FIRST_CHANNEL];
+            std::vector<double>* channel = channels[j];
+            double average = getMean(channel);
 
-            double average = std::accumulate(channel->end() - NUM_AVERAGE,
-                channel->end(), 0) / NUM_AVERAGE;
+            std::vector<double> event = getInterestZone(channel);
 
-            int peak = std::max_element(channel->begin(),
-                channel->end()) - channel->begin();
-            auto begin = channel->begin() + peak - BEFORE_PEAK;
-            auto end = channel->begin() + peak + AFTER_PEAK + 1;
-            std::vector<double> event(begin, end);
+            TGraph graph(NUM_SAMPLES, &time[0], &event[0]);
+            fit.SetParameter(0, average);
+            fit.SetParLimits(0, average, average + 2000);
+            gSystem->RedirectOutput("/dev/null");
+            graph.Fit("fit", "NMQS");
+            gSystem->RedirectOutput(0, 0);
 
-            TGraph *graph = new TGraph(NUM_SAMPLES, &time[0], &event[0]);
-            graph->Fit(fit, "MNQ");
-            amplitudes[j - FIRST_CHANNEL] = fit->GetParameter(0) - average;
+            amplitudes[j - FIRST_CHANNEL] = fit.GetParameter(0) - average;
+            if(fit.GetParameter(0) - average > 2000 || fit.GetParameter(0) < 0) {
+                std::cout << fit.GetParameter(0) << std::endl;
+                return;
+            }
         }
 
         out->Fill();
@@ -82,19 +125,27 @@ void analyze(TTree *in, TTree *out) {
             out->Write();
         }
     }
+
     out->Write();
+    out->ResetBranchAddresses();
     in->ResetBranchAddresses();
 }
 
 void preprocessing() {
-    TFile *in = TFile::Open(IN_PATH.c_str(), "READ");
-    TTree *tree = (TTree*) in->Get("wfm");
+    TFile *in = TFile::Open("/media/work/Waveforms/run4/100-200.root", "READ");
+    TTree *wfm = (TTree*) in->Get("wfm");
 
-    TFile *out = TFile::Open(OUT_PATH.c_str(), "CREATE");
-    TTree *data = new TTree("data", "Analysis data");
+    TFile *out; TTree *data;
+    if(APPEND) {
+        out = TFile::Open(OUT_PATH.c_str(), "UPDATE");
+        data = (TTree*) out->Get("data");
+    } else {
+        out = TFile::Open(OUT_PATH.c_str(), "CREATE");
+        data = new TTree("data", "Analysis data");
+    }
 
-    analyze(tree, data);
+    analyze(wfm, data);
 
-    out->Close();
     in->Close();
+    out->Close();
 }
